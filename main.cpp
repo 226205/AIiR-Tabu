938fd86e280e3fcdab0ead26b1e6c0c2a0fe5d37
226205 <aborecki@op.pl>
2019-04-28 21:57:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,23 @@
 #include <ctime>
 
 
-int cityamount;
+int cityamount =3;
 int **distances;
 
 
 bool fileread();
+bool result(int);
+void filewrite(int*, int);
 void writetab(int**);
 void TabuSearch();
 
 int  main()
 {
     srand(time(NULL));
-    fileread();
-    TabuSearch();
+    if(fileread() == 0)
+        std::cout << "\n OTWIERANIE PLIKU Z MACIERZA NIE POWIODLO SIE!!!";
+    else
+        TabuSearch();
 }
 
 
@@ -36,10 +40,47 @@ bool fileread()         // funkcja do odczytywania danych z pliku
         for(int i=0;i<cityamount;++i)       // zapelnianie tablicy danymi z pliku
             for(int j=0;j<cityamount;++j)
                 plik>>distances[i][j];
+        plik.close();
         return 1;
     }
+    plik.close();
     return 0;
 }
+
+bool result(int cost)         // funkcja do zapisywania danych do pliku
+{
+    int oldCost;
+    std::string oldName, currentName = "ftv47.atsp";
+    std::string filename = "wynik.txt";
+    std::fstream plik;
+    plik.open(filename,std::ios::in);
+    if(plik.good())                         // sprawdzenie poprawnosci wczytania pliku, w razie niepowodzenia zwracamy blad
+    {
+        plik>>oldName;
+        plik>>oldCost;
+        plik.close();
+        if(oldCost > cost || oldName.compare(currentName)){
+            std::cout << "\nsprawdzam";
+            return 1;
+        }
+        return 0;
+    }
+    plik.close();
+    return 1;
+}
+
+void filewrite(int* sequence, int cost)
+{
+    std::ofstream ofs;
+    ofs.open("wynik.txt", std::ofstream::out | std::ofstream::trunc);
+    ofs<<"ftv47.atsp"<<" "<<cost<<" ";
+    for(int i = 0; i < cityamount; i++)
+        ofs<<sequence[i]<<"->";
+    ofs<<sequence[cityamount];
+    ofs.close();
+}
+
+
 
 void writetab(int** cities)                 // wypisywanie aktualnej macierzy przejsc miedzy miastami
 {
@@ -76,8 +117,8 @@ void TabuSearch()
 
     int* tempPath = new int [2];                    // miasta aktualnie badanego sasiedztwa
 
-    int tempCost = INT_MAX;
     int bestResult = INT_MAX;
+    int tempCost = INT_MAX;
     int tempV, temp = 0;
 
     bool* cityB = new bool[cityamount + 1];            //stworzenie tablicy mowiacej czy dana liczba juz wystapila w sekwencji
@@ -114,13 +155,100 @@ void TabuSearch()
     for (int i = 0; i < cityamount + 1; i++)
         bestPath[i] = tempBestPath[i];
 
+//tmp2 = distances[newGeneration[q][i-1]][newGeneration[q][j]] + distances[newGeneration[q][j]][newGeneration[q][i]] + distances[newGeneration[q][i]][newGeneration[q][j+1]] - distances[newGeneration[q][i-1]][newGeneration[q][i]] - distances[newGeneration[q][i]][newGeneration[q][j]] - distances[newGeneration[q][j]][newGeneration[q][j+1]];
+//tmp2 = distances[newGeneration[q][i-1]][newGeneration[q][j]] + distances[newGeneration[q][j]][newGeneration[q][i+1]] + distances[newGeneration[q][j-1]][newGeneration[q][i]] + distances[newGeneration[q][i]][newGeneration[q][j+1]] - distances[newGeneration[q][i-1]][newGeneration[q][i]] - distances[newGeneration[q][i]][newGeneration[q][i+1]] - distances[newGeneration[q][j-1]][newGeneration[q][j]] - distances[newGeneration[q][j]][newGeneration[q][j+1]];
+
+    do{
+        iteration++;
+
+        bestResult= INT_MAX;
+        for(int i = 1; i < cityamount-1; i++)
+            for(int j = i+1; j < cityamount; j++){
+
+                tempCost = 0;
+                // wyliczanie roznicy w koszcie, obliczajac jedynie roznice miedzy sciezkami bedacymi czesciami zamienionych miast
+                if(i+1 == j){
+                    tempCost = distances[tempBestPath[i-1]][tempBestPath[j]] + distances[tempBestPath[j]][tempBestPath[i]] + distances[tempBestPath[i]][tempBestPath[j+1]] - distances[tempBestPath[i-1]][tempBestPath[i]] - distances[tempBestPath[i]][tempBestPath[j]] - distances[tempBestPath[j]][tempBestPath[j+1]];
+    //                        std::cout << "\n " << i << "  " << j << "  cost: " << tempCost << " = " << distances[tempBestPath[i-1]][tempBestPath[j]]  << " + " << distances[tempBestPath[j]][tempBestPath[i]] << " + " << distances[tempBestPath[i]][tempBestPath[j+1]] << " - " << distances[tempBestPath[i-1]][tempBestPath[i]] << " - " << distances[tempBestPath[i]][tempBestPath[j]] << " - " << distances[tempBestPath[j]][tempBestPath[j+1]];
+                }
+                else{
+                    tempCost = distances[tempBestPath[i-1]][tempBestPath[j]] + distances[tempBestPath[j]][tempBestPath[i+1]] + distances[tempBestPath[j-1]][tempBestPath[i]] + distances[tempBestPath[i]][tempBestPath[j+1]] - distances[tempBestPath[i-1]][tempBestPath[i]] - distances[tempBestPath[i]][tempBestPath[i+1]] - distances[tempBestPath[j-1]][tempBestPath[j]] - distances[tempBestPath[j]][tempBestPath[j+1]];
+    //                        std::cout << "\n " << i << "  " << j << "  cost: " << tempCost << " = " << distances[tempBestPath[i-1]][tempBestPath[j]] << " + " << distances[tempBestPath[j]][tempBestPath[i+1]] << " + " << distances[tempBestPath[j-1]][tempBestPath[i]] << " + " << distances[tempBestPath[i]][tempBestPath[j+1]] << " - " << distances[tempBestPath[i-1]][tempBestPath[i]] << " - " << distances[tempBestPath[i]][tempBestPath[i+1]] << " - " << distances[tempBestPath[j-1]][tempBestPath[j]] << " - " << distances[tempBestPath[j]][tempBestPath[j+1]];
+                }
+
+    //                    std::cout << "\n j: " << tempBestPath[j] << " i: " << tempBestPath[i];        //wypisywanie numerow zamienionych miast aby poprawnie uzyc tabuList
+                if(tempBestPath[i] > tempBestPath[j]){                                          //tabuList dziala tylko po jednej stronie przekatnej macierzy przejsc, oby oszczedzic na ilosci operacji. wybieramy te strone realizujac podanego ifa
+    //                        std::cout << "  j < i";
+                    if(tabuList[tempBestPath[j]][tempBestPath[i]] < iteration || tempCost < 0){      //sprawdzenie czy wygenerowany sasiad nie nalezy do tabu lub spelnia kryterium aspiracji
+                        if(tempCost < bestResult){                                             //sprawdzenie czy wygenerowany sasiad jest lepszy od juz sprawdzonych
+                            tempPath[0] = i;
+                            tempPath[1] = j;
+                            bestResult = tempCost;
+                    }    }
+                }
+                else{                                                                           //to samo co w if z ta roznica, ze zostala wybrana komorka tablicy znajduja sie symetrycznie do przekatnej macierzy, aby uzywac tylko polowy tablicy
+    //                        std::cout << "  j > i";
+                    if(tabuList[tempBestPath[i]][tempBestPath[j]] < iteration || tempCost < 0)
+                        if(tempCost < bestResult){
+                            tempPath[0] = i;
+                            tempPath[1] = j;
+                            bestResult = tempCost;}
+                }
+            }
+
+        temp = tempBestPath[tempPath[0]];                               //zamiana najlepszych miast miejscami
+        tempBestPath[tempPath[0]] = tempBestPath[tempPath[1]];
+        tempBestPath[tempPath[1]] = temp;
+
+//            std::cout << "\n\n";                                        //sprawdzanie nowej, najlepszej sciezki
+//            for(int i = 0; i < cityamount; i++)
+//                std::cout << tempBestPath[i] << " -> ";
+//            std::cout << tempBestPath[cityamount] << "  tempBestCost: " << tempBestCost << "\n zamiana miejsca: " << tempPath[0] << " z " << tempPath[1];
+
+//        temp = 0;
+//        for(int i = 0; i <cityamount; i++)
+//            temp += distances[tempBestPath[i]][tempBestPath[i + 1]];
+//            std::cout << "   Wyliczony nowy koszt: " << temp;         //wypis kosztu nowej sciezki
+
+//            tempBestCost = temp;
+        tempBestCost += bestResult;
 
 
-    std::cout<<"\n\nNajkrotsza odnaleziona droga przez wszystkie miasta to:\n";           // wypisanie wyników procesu
-    for(int i = 0; i < cityamount; i++)
-        std::cout << bestPath[i] << " -> ";
-    std::cout << bestPath[cityamount];
-    std::cout<<"\nJej calkowity dystans wynosi: " << bestCost;
+        if(tempBestPath[tempPath[0]] < tempBestPath[tempPath[1]])                           // dodanie nowego przejscia do tabuList o wartosci iteration + tabuSize, czyli aktualnego numeru przejscia plus ile jeszcze dana rzecz ma pozostac w tabu
+            tabuList[tempBestPath[tempPath[0]]][tempBestPath[tempPath[1]]] = tabuSize + iteration;
+        else
+            tabuList[tempBestPath[tempPath[1]]][tempBestPath[tempPath[0]]] = tabuSize + iteration;
+
+
+//        std::cout << "\n\n";
+//        for(int i = 1; i < cityamount; i++){               // wypisanie tabuList
+//            for(int j = 1; j < cityamount; j++){
+//                std::cout << tabuList[i][j] << " ";}
+//           std::cout << "\n";}
+
+        if( bestCost > tempBestCost)                    //zapisanie nowego najlepszego wyniku, badz zaznaczenie kolejnego z rzedu przejscia petli bez niego, aby wywolac funkcje dywersyfikacji
+        {
+            bestCost = tempBestCost;
+            iterationWoImprovement = 0;
+            for (int i = 0; i < cityamount + 1; i++)
+                bestPath[i] = tempBestPath[i];
+        }
+        else
+            iterationWoImprovement++;
+
+    }while(iterationWoImprovement < maxIterationWoImprovement);
+
+
+//    std::cout<<"\n\nNajkrotsza odnaleziona droga przez wszystkie miasta to:\n";           // wypisanie wyników procesu na ekran
+//    for(int i = 0; i < cityamount; i++)
+//        std::cout << bestPath[i] << " -> ";
+//    std::cout << bestPath[cityamount];
+//    std::cout<<"\nJej calkowity dystans wynosi: " << bestCost;
+
+    if(result(bestCost) == 1){
+        std::cout<<"\nPrzekazujemy wynik";
+        filewrite(bestPath, bestCost);
+    }
 
     for(int i = 0; i < cityamount; i++)
         delete[] tabuList[i];
@@ -130,3 +258,5 @@ void TabuSearch()
 	delete[] tempPath;
     delete[] cityB;
 }
+
+
